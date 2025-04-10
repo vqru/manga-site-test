fb7831d0aea850e00c03b8241115107e65aea60b
vqru <154732468+vqru@users.noreply.github.com>
2025-04-10 15:47:12 +0530

    Update fetchChapterData.js

--- netlify/functions/fetchChapterData.js
+++ netlify/functions/fetchChapterData.js
@@ -2,14 +2,17 @@ const axios = require('axios');
 
 exports.handler = async (event) => {
   const { id } = event.queryStringParameters;
-  const apiUrl = `https://api.mangadex.org/chapter/${id}?includes[]=scanlation_group`;
+  const apiUrl = `https://api.mangadex.org/chapter/${id}?includes[]=scanlation_group&includes[]=manga`;
 
   try {
     const response = await axios.get(apiUrl);
     return {
       statusCode: 200,
       headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify(response.data.data)
+      body: JSON.stringify({
+        chapter: response.data.data,
+        mangaId: response.data.data.relationships.find(r => r.type === 'manga').id
+      })
     };
   } catch (error) {
     return {
