36cccac8362dedeb18b423d2df8f6b5b35af70f0
vqru <154732468+vqru@users.noreply.github.com>
2025-04-10 12:12:16 +0530

    Update fetchMangaDetails.js

--- netlify/functions/fetchMangaDetails.js
+++ netlify/functions/fetchMangaDetails.js
@@ -2,8 +2,9 @@ const axios = require('axios');
 
 exports.handler = async (event) => {
   const { id } = event.queryStringParameters;
-  const mangaUrl = `https://api.mangadex.org/manga/${id}?includes[]=cover_art&includes[]=author`;
-const chaptersUrl = `https://api.mangadex.org/manga/${id}/feed?limit=100&order[chapter]=desc&translatedLanguage[]=en`;
+  const mangaUrl = `https://api.mangadex.org/manga/${id}?includes[]=cover_art`;
+  // Get LATEST English chapters first, including unvolumed chapters
+  const chaptersUrl = `https://api.mangadex.org/manga/${id}/feed?limit=100&order[updatedAt]=desc&translatedLanguage[]=en`;
 
   try {
     const [mangaRes, chaptersRes] = await Promise.all([
@@ -13,7 +14,7 @@ const chaptersUrl = `https://api.mangadex.org/manga/${id}/feed?limit=100&order[c
 
     return {
       statusCode: 200,
-      headers: { 'Access-Control-Allow-Origin': '*' },
+      headers: { "Access-Control-Allow-Origin": "*" },
       body: JSON.stringify({
         manga: mangaRes.data.data,
         chapters: chaptersRes.data.data
@@ -22,7 +23,7 @@ const chaptersUrl = `https://api.mangadex.org/manga/${id}/feed?limit=100&order[c
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: error.message })
+      body: JSON.stringify({ error: "API failed" })
     };
   }
 };
